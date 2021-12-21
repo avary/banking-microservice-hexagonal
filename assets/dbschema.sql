9dc66d607dd806a1c64dce65c0cef4fe68f4e5a7
Ashef Habib Tishad <ashefhabib@outlook.com>
2021-12-21 08:56:40 +0600

    Unnecessary files removed

---
+++ assets/dbschema.sql
@@ -0,0 +1,69 @@
+SET SCHEMA 'banking';
+
+CREATE TABLE "customers" (
+  "customer_id" bigserial PRIMARY KEY NOT NULL,
+  "name" varchar NOT NULL,
+  "date_of_birth" timestamp DEFAULT (now()),
+  "city" varchar NOT NULL,
+  "zipcode" varchar NOT NULL,
+  "status" SMALLINT NOT NULL DEFAULT 1
+);
+
+CREATE TABLE "accounts" (
+  "account_id" bigserial PRIMARY KEY NOT NULL,
+  "customer_id" bigint NOT NULL,
+  "opening_date" timestamp NOT NULL DEFAULT 'now()',
+  "account_type" varchar NOT NULL,
+  "amount" decimal NOT NULL,
+  "status" SMALLINT NOT NULL DEFAULT 1
+);
+
+CREATE TABLE "transactions" (
+  "transaction_id" bigserial PRIMARY KEY NOT NULL,
+  "account_id" bigint NOT NULL,
+  "amount" decimal NOT NULL,
+  "transaction_type" varchar NOT NULL,
+  "transaction_date" timestamp NOT NULL DEFAULT 'now()'
+);
+
+CREATE TABLE "users" (
+  "username" varchar PRIMARY KEY,
+  "password" varchar NOT NULL,
+  "role" varchar NOT NULL,
+  "customer_id" bigint NOT NULL,
+  "created_on" timestamp NOT NULL DEFAULT 'now()'
+);
+
+ALTER TABLE "accounts" ADD FOREIGN KEY ("customer_id") REFERENCES "customers" ("customer_id");
+
+ALTER TABLE "transactions" ADD FOREIGN KEY ("account_id") REFERENCES "customers" ("customer_id");
+
+ALTER TABLE "users" ADD FOREIGN KEY ("customer_id") REFERENCES "customers" ("customer_id");
+
+ALTER SEQUENCE customers_customer_id_seq RESTART WITH 2006;
+
+ALTER SEQUENCE accounts_account_id_seq RESTART WITH 95476;
+
+
+
+CREATE INDEX ON "customers" ("status");
+
+CREATE INDEX ON "customers" ("customer_id");
+
+CREATE INDEX ON "customers" ("name");
+
+CREATE INDEX ON "accounts" ("customer_id");
+
+CREATE INDEX ON "accounts" ("account_id");
+
+CREATE INDEX ON "accounts" ("account_type");
+
+CREATE INDEX ON "accounts" ("status");
+
+CREATE INDEX ON "transactions" ("account_id");
+
+CREATE INDEX ON "transactions" ("transaction_type");
+
+CREATE INDEX ON "users" ("username");
+
+CREATE INDEX ON "users" ("role");
